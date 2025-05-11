e22833a8199534a7a6bc3b6f5e2277c0060d1b5b
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-11 19:51:19 +0200

    Handling uncaughtException, unhandledRejection errors and SIGTERM signal closing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}...`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  } else {
+    process.exit();
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtExeprion", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  }
 });
